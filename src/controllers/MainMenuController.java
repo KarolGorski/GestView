bcd0962591521d12d895c500892e27e9d14478b9
Karol Górski <goralk2870@gmail.com>
2019-01-26 20:42:57 +0100

    Directory picker is working

--- src/controllers/MainMenuController.java
+++ src/controllers/MainMenuController.java
@@ -1,4 +1,29 @@
 package controllers;
 
+import app.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+
 public class MainMenuController {
+
+    @FXML
+    Button pickPathButton;
+    @FXML
+    TextField pickedPathTextField;
+
+    public void pickPathButtonAction(){
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Images directory");
+        File defaultDirectory = new File("./");
+        chooser.setInitialDirectory(defaultDirectory);
+        File selectedDirectory = chooser.showDialog(Main.stage);
+        System.out.println("STH WORKS?");
+
+        pickedPathTextField.setText(selectedDirectory.getAbsolutePath());
+    }
+
 }
